28ebdcea4f0404bd9deeb88a0f28cdcafd793ddc
Marian Ban <marian.ban@gmail.com>
2017-03-25 17:21:17 +0100

    chapter 14 finished

--- chapter.14/Chapter Exercies/ciphers/app/Main.hs
+++ chapter.14/Chapter Exercies/ciphers/app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
+import Test.QuickCheck
 import Lib
 
+test :: [Char]
+test = "&[];/,`%₉"
+
+
+
 main :: IO ()
-main = putStrLn "Main not yet implemented"
+main = putStrLn (show (test == (vigenereDecode "keyword" (vigenere "keyword" test))))
--- chapter.14/Chapter Exercies/ciphers/test/Spec.hs
+++ chapter.14/Chapter Exercies/ciphers/test/Spec.hs
@@ -8,6 +8,15 @@ ceasarTestCase (Positive x) = "test" == (cipher (-x) (cipher x "test"))
 vigenereTestCase :: [Char] -> Bool
 vigenereTestCase xs = xs == (vigenereDecode "keyword" (vigenere "keyword" xs))
 
+genLetter :: Gen Char
+genLetter = elements "abcdeafghjklmnoprsqutvxyz123456789"
+
+genWords :: Gen [Char]
+genWords = resize 20 (listOf genLetter)
+
+vingenereProperty :: Property
+vingenereProperty = forAll genWords vigenereTestCase
+
 main :: IO ()
 main = hspec $ do
   describe "Ceasar cipher" $ do
@@ -15,4 +24,4 @@ main = hspec $ do
       property ceasarTestCase
   describe "Vigenere cipher" $ do
     it "works for arbitrary string key" $ do
-      property vigenereTestCase
+      vingenereProperty
