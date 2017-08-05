0aa367f1d7f3321de87d8adffd0b63d03e02be5d
Marian Ban <marian.ban@gmail.com>
2017-08-05 15:08:16 +0200

    chapter 19 exercises

---
+++ chapter.18/ApplicationAndComposition.hs
@@ -0,0 +1,16 @@
+module ApplicationAndComposition where
+  import Control.Monad ((>=>))
+
+  sayHi :: String -> IO String
+  sayHi greeting = do
+    putStrLn greeting
+    getLine
+
+  readM :: Read a => String -> IO a
+  readM = return . read
+
+  getAge :: String -> IO Int
+  getAge = sayHi >=> readM
+
+  askForAge :: IO Int
+  askForAge = getAge "Hello! How old are you? "
